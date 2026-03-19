bb4459a1936f428c346401052f8575efaa875840
Chaitanya <chaitanyachaitu157@gmail.com>
2026-03-19 18:49:28 +0530

    updates

--- timestamp.sh
+++ timestamp.sh
@@ -4,10 +4,10 @@ startime=$(date + %S)
 
 echo "script executed at $startime"
 
-sleep=10
+sleep 10
 
 endtime=$(date + %S)
  
-total_time=(($endtime-$startime))
+total_time=$(($endtime-$startime))
 
 echo "script excuted in $total_time"

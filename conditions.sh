1b2c5b2ab9de5ec6e22e46e64ab306353e002fbb
Chaitanya <chaitanyachaitu157@gmail.com>
2026-03-19 19:52:03 +0530

    updates

---
+++ conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+number=7
+
+if [ $number -gt 20 ]; then
+echo "given number is greater than 20"
+
+elif [ $number -eq 7 ];then
+echo "equal number"
+
+else 
+echo "give number is smaller than 20"
+fi

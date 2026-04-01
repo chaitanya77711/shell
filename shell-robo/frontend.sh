1f30232f7ca120860319b1e920a2a099ebdc6c94
Chaitanya <chaitanyachaitu157@gmail.com>
2026-04-01 23:08:18 +0530

    updates

--- shell-robo/frontend.sh
+++ shell-robo/frontend.sh
@@ -8,51 +8,49 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 script_dir=$(pwd)
-MONGODB_HOST=devops7.online
-
 
 if [ $user_id -ne 0 ]; then
   echo -e $R "run with root user" $N | tee -a $logs_file
   exit 1
-
 fi  
 
 mkdir -p $logs_folder
 
 validate() {
     if [ $1 -ne 0 ]; then
-     echo -e $R  "$2 failure" $N | tee -a $logs_file
+     echo -e $R "$2 failure" $N | tee -a $logs_file
      exit 1
-
     else
-     echo -e $G  "$2 success" $N | tee -a $logs_file
-
+     echo -e $G "$2 success" $N | tee -a $logs_file
     fi  
-
 } 
 
+# install nginx
 dnf module disable nginx -y &>>$logs_file
 dnf module enable nginx:1.24 -y &>>$logs_file
 dnf install nginx -y &>>$logs_file
 validate $? "Installing Nginx"
 
+# start nginx
 systemctl enable nginx &>>$logs_file
 systemctl start nginx
 validate $? "Enabled and started nginx"
 
+# remove old content
 rm -rf /usr/share/nginx/html/*
 validate $? "Remove default content"
 
+# download frontend
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$logs_file
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$logs_file
 validate $? "Downloaded and unzipped frontend"
 
-rm -rf /etc/nginx/nginx.conf
-
+# ✅ IMPORTANT FIX (ikkade main change)
 cp $script_dir/nginx.conf /etc/nginx/default.d/roboshop.conf
 validate $? "Copied our nginx conf file"
 
+# restart nginx
 systemctl restart nginx
 validate $? "Restarted Nginx"
